90861f8cd3cb5cc7c6f2cf59c77d852b894a426d
Florian Lamml <info@florian-lamml.de>
2014-10-24 10:30:05 +0200

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Florian Lamml <info@florian-lamml.de>
+ */
+$lang['sapnoteurl']            = 'La note SAP URL, par défaut https://service.sap.com/sap/support/notes';
