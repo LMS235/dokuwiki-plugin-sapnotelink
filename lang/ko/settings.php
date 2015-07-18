40091ac874b27cb9b98e4703fdf64107f840753d
Myeongjin <aranet100@gmail.com>
2015-07-18 19:10:05 +0200

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['sapnoteurl']            = 'SAP 노트 URL, 기본값 https://service.sap.com/sap/support/notes';
