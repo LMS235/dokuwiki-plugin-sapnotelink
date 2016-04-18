d24600d4d0d77fef1568f0421ed04666ff4dc90d
LMS235 <lms23@web.de>
2016-04-18 15:01:06 +0200

    Conf

--- conf/default.php
+++ conf/default.php
@@ -2,5 +2,6 @@
 /**
  *	Options for the sapnotelink Plugin
  */
+ 
 $conf['sapnoteurl'] = 'https://service.sap.com/sap/support/notes';	//SAP Note URL
 $cont['sapnotelink_toolbar_icon'] = 0;	// Toolbar Icon YES/NO
--- conf/metadata.php
+++ conf/metadata.php
@@ -1,8 +1,7 @@
 <?php
 /**
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
- * @author Florian Lamml <info@florian-lamml.de>
+ *	Options for the sapnotelink Plugin
  */
+ 
 $meta['sapnoteurl'] = array('string');	//SAP Note URL
 $meta['sapnotelink_toolbar_icon'] = array('onoff');	// Toolbar Icon YES/NO
