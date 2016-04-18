1a8920bed0332983be3e40314611351edc2e6059
LMS235 <lms23@web.de>
2016-04-18 15:06:20 +0200

    Conf

--- conf/default.php
+++ conf/default.php
@@ -3,5 +3,6 @@
  *	Options for the sapnotelink Plugin
  */
  
-$conf['sapnoteurl'] = 'https://service.sap.com/sap/support/notes';	//SAP Note URL
+$conf['sapnoteurl'] = 'https://service.sap.com/sap/support/notes';	// SAP Note URL
+
 $cont['sapnotelink_toolbar_icon'] = 0;	// Toolbar Icon YES/NO
--- conf/metadata.php
+++ conf/metadata.php
@@ -3,5 +3,6 @@
  *	Options for the sapnotelink Plugin
  */
  
-$meta['sapnoteurl'] = array('string');	//SAP Note URL
+$meta['sapnoteurl'] = array('string');	// SAP Note URL
+
 $meta['sapnotelink_toolbar_icon'] = array('onoff');	// Toolbar Icon YES/NO
