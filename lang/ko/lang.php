b4ea8213adac95f0d0a920b9de528aaced055e57
Myeongjin <aranet100@gmail.com>
2016-04-19 12:25:06 +0200

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -1,7 +1,9 @@
 <?php
+
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Myeongjin <aranet100@gmail.com>
  */
 $lang['url_alt']               = 'SAP 노트';
+$lang['toolbar_icon']          = 'SAP 노드 넣기';
--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -1,7 +1,9 @@
 <?php
+
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Myeongjin <aranet100@gmail.com>
  */
 $lang['sapnoteurl']            = 'SAP 노트 URL, 기본값 https://service.sap.com/sap/support/notes';
+$lang['sapnotelink_toolbar_icon'] = '도구 모음 아이콘 보이기';
