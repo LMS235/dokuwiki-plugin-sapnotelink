ecd26f5cdb5054fa7f1538955875c42ebf208c94
None <None@none.no>
2016-04-19 12:10:05 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,7 +1,10 @@
 <?php
+
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Florian Lamml <info@florian-lamml.de>
+ * @author None <None@none.no>
  */
 $lang['url_alt']               = 'la note SAP';
+$lang['toolbar_icon']          = 'Insérer la note SAP';
