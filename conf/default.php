070279ee0074b3027da04a529441f57e1eebb58a
LMS235 <lms23@web.de>
2016-06-17 10:01:14 +0200

    new sapnote url (new Launchpad)
    move from Legacy Applications (https://service.sap.com/sap/support/notes) to the new Launchpad (https://launchpad.support.sap.com/#/notes)

--- conf/default.php
+++ conf/default.php
@@ -5,5 +5,6 @@
  * @author Florian Lamml <info@florian-lamml.de>
  */
 
-$conf['sapnoteurl'] = 'https://service.sap.com/sap/support/notes';	// SAP Note URL
+// $conf['sapnoteurl'] = 'https://service.sap.com/sap/support/notes';	// SAP Note URL (Legacy Applications)
+$conf['sapnoteurl'] = 'https://launchpad.support.sap.com/#/notes';	// SAP Note URL (new Launchpad)
 $conf['sapnotelink_toolbar_icon'] = 0;	// Toolbar Icon YES/NO
