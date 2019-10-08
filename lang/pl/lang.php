c9ca8787841ced2e240f13553406062be098f207
Bartek S <sadupl@gmail.com>
2019-10-08 20:10:35 +0200

    translation update

---
+++ lang/pl/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['url_alt']               = 'notatka SAP';
+$lang['toolbar_icon']          = 'Wstaw notatkę SAP';
