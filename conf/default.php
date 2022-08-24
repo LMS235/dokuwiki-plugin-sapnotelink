83a5a50e45b65c85c37e65edb9e0bea2b7863946
LMS235 <lms23@web.de>
2022-08-24 08:39:54 +0200

    SAP for Me Link
    SAP for Me Link

--- conf/default.php
+++ conf/default.php
@@ -6,5 +6,6 @@
  */
 
 // $conf['sapnoteurl'] = 'https://service.sap.com/sap/support/notes';	// SAP Note URL (Legacy Applications)
-$conf['sapnoteurl'] = 'https://launchpad.support.sap.com/#/notes';	// SAP Note URL (new Launchpad)
+// $conf['sapnoteurl'] = 'https://launchpad.support.sap.com/#/notes';	// SAP Note URL (new Launchpad)
+$conf['sapnoteurl'] = 'https://me.sap.com/notes';	// SAP Note URL (new SAP for Me)
 $conf['sapnotelink_toolbar_icon'] = 0;	// Toolbar Icon YES/NO
