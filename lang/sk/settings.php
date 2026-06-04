f750d11f0f3f74ff8054c93fd10795b5b2640fe3
Wizzard <wizzardsk@gmail.com>
2026-06-04 21:55:35 +0200

    Translation update (sk)

---
+++ lang/sk/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['sapnoteurl']            = 'URL SAP note, predvolene https://service.sap.com/sap/support/notes';
+$lang['sapnotelink_toolbar_icon'] = 'Zobraziť ikonu v paneli nástrojov';
